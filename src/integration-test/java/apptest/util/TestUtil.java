2f7278f96f7c1d21efa36f2d9bb120c2a9857d8a
Jonas Klingstedt <jonas.klingstedt@gmail.com>
2024-01-31 14:02:43 +0100

    UF-5739 - App tests (#9)
    Adds basic app tests

---
+++ src/integration-test/java/apptest/util/TestUtil.java
@@ -0,0 +1,52 @@
+package apptest.util;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
+import org.apache.commons.compress.compressors.lzma.LZMACompressorInputStream;
+import org.apache.commons.compress.utils.IOUtils;
+
+public final class TestUtil {
+
+    private TestUtil() { }
+
+    public static Map<String, byte[]> extractZipFile(final File file) throws IOException {
+        var decompressedData = decompress7zFile(file);
+
+        var entries = new HashMap<String, byte[]>();
+        try (var byteArrayInputStream = new ByteArrayInputStream(decompressedData);
+             var zipArchiveInputStream = new ZipArchiveInputStream(byteArrayInputStream)) {
+            var zipEntry = zipArchiveInputStream.getNextEntry();
+            while (zipEntry != null) {
+                entries.put(zipEntry.getName(), extractZipEntryData(zipArchiveInputStream));
+
+                zipEntry = zipArchiveInputStream.getNextEntry();
+            }
+        }
+
+        return entries;
+    }
+
+    private static byte[] decompress7zFile(final File file) throws IOException {
+        var out = new ByteArrayOutputStream();
+        try (var outFileInputStream = new FileInputStream(file);
+             var lzmaInputStream = new LZMACompressorInputStream(outFileInputStream)) {
+            IOUtils.copy(lzmaInputStream, out);
+        }
+        return out.toByteArray();
+    }
+
+    private static byte[] extractZipEntryData(final ZipArchiveInputStream in) throws IOException {
+        try (var out = new ByteArrayOutputStream()) {
+            IOUtils.copy(in, out);
+
+            return out.toByteArray();
+        }
+    }
+}
