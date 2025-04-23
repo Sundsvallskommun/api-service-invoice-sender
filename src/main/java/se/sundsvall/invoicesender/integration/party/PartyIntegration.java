ac4b1a0095b883157209d4de5821bbca72d88f95
AndreSoderlund <andre.soderlund@b3.se>
2025-04-23 12:50:02 +0200

    Updated call to party, sets the legalId based on a calculated guess instead of testing both 20th and 21st century.

--- src/main/java/se/sundsvall/invoicesender/integration/party/PartyIntegration.java
+++ src/main/java/se/sundsvall/invoicesender/integration/party/PartyIntegration.java
@@ -2,6 +2,7 @@ package se.sundsvall.invoicesender.integration.party;
 
 import static generated.se.sundsvall.party.PartyType.PRIVATE;
 
+import java.time.LocalDate;
 import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,18 +25,24 @@ public class PartyIntegration {
 		// Strip everything but digits from the legal id
 		var legalIdWithDigitsOnly = legalId.replaceAll("\\D", "");
 
-		try {
-			// Use the stripped legal id as-is, if it already starts with 19 or 20
-			if (legalIdWithDigitsOnly.startsWith("19") || legalIdWithDigitsOnly.startsWith("20")) {
-				return partyClient.getPartyId(municipalityId, PRIVATE, legalIdWithDigitsOnly).map(partyId -> new LegalIdAndPartyId(legalIdWithDigitsOnly, partyId));
-			}
-			return partyClient.getPartyId(municipalityId, PRIVATE, "19" + legalIdWithDigitsOnly)
-				.map(partyId -> new LegalIdAndPartyId("19" + legalIdWithDigitsOnly, partyId))
-				.or(() -> partyClient.getPartyId(municipalityId, PRIVATE, "20" + legalIdWithDigitsOnly)
-					.map(partyId -> new LegalIdAndPartyId("20" + legalId, partyId)));
-		} catch (final Exception e) {
-			LOG.info("Unable to get party id for legal id [19|20]{}: {}", legalIdWithDigitsOnly, e.getMessage());
+		var currentYear = LocalDate.now().getYear();
+		// Example legal id : 900101-1234. Birth year is the first two digits of the legal id.
+		var birthYear = Integer.parseInt(legalIdWithDigitsOnly.substring(0, 2));
 
+		// Example currentYear : 2023. 2023 % 100 = 23. If birthYear is greater than currentYear % 100, then the birth year is
+		// in the 1900s.
+		// This solution is flawed for people older than 100.
+		String legalIdWithBirthYear;
+		if (birthYear > currentYear % 100) {
+			legalIdWithBirthYear = "19".concat(legalIdWithDigitsOnly);
+		} else {
+			legalIdWithBirthYear = "20".concat(legalIdWithDigitsOnly);
+		}
+
+		try {
+			return partyClient.getPartyId(municipalityId, PRIVATE, legalIdWithBirthYear).map(partyId -> new LegalIdAndPartyId(legalIdWithBirthYear, partyId));
+		} catch (final Exception e) {
+			LOG.info("Unable to get party id for legal id {}: {}", legalIdWithBirthYear, e.getMessage());
 			return Optional.empty();
 		}
 	}
--- src/test/java/se/sundsvall/invoicesender/integration/party/PartyIntegrationTests.java
+++ src/test/java/se/sundsvall/invoicesender/integration/party/PartyIntegrationTests.java
@@ -3,7 +3,6 @@ package se.sundsvall.invoicesender.integration.party;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
@@ -44,22 +43,6 @@ class PartyIntegrationTests {
 	}
 
 	@Test
-	void testGetPartyIdWhenNothingIsFoundForFirstAttempt() {
-		when(mockPartyClient.getPartyId(any(String.class), eq(PartyType.PRIVATE), any(String.class)))
-			.thenReturn(Optional.empty()).thenReturn(Optional.of("somePartyId"));
-
-		var legalIdAndPartyId = partyIntegration.getPartyId("2505158888", "2281");
-
-		assertThat(legalIdAndPartyId).isPresent().hasValueSatisfying(actualValue -> {
-			assertThat(actualValue.legalId()).isEqualTo("202505158888");
-			assertThat(actualValue.partyId()).isEqualTo("somePartyId");
-		});
-
-		verify(mockPartyClient, times(2)).getPartyId(any(String.class), eq(PartyType.PRIVATE), any(String.class));
-		verifyNoMoreInteractions(mockPartyClient);
-	}
-
-	@Test
 	void testGetPartyIdWhenNothingIsFound() {
 		when(mockPartyClient.getPartyId(any(String.class), eq(PartyType.PRIVATE), any(String.class)))
 			.thenReturn(Optional.empty());
@@ -68,7 +51,7 @@ class PartyIntegrationTests {
 
 		assertThat(partyId).isEmpty();
 
-		verify(mockPartyClient, times(2)).getPartyId(any(String.class), eq(PartyType.PRIVATE), any(String.class));
+		verify(mockPartyClient).getPartyId(any(String.class), eq(PartyType.PRIVATE), any(String.class));
 		verifyNoMoreInteractions(mockPartyClient);
 	}
 
